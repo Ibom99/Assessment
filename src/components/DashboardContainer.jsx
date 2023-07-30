c7aeb20353e802df10656108b8721db3072e7c37
Ibom99 <ibom.esther@gmail.com>
2023-07-30 17:21:41 +0100

    modified: files

--- src/components/DashboardContainer.jsx
+++ src/components/DashboardContainer.jsx
@@ -1,41 +1,48 @@
-import React from 'react'
-import '../styles/DashboardContainer.css'
+import React from "react";
+import "../styles/DashboardContainer.css";
 
 const DashboardContainer = () => {
   return (
-    <div className='dashboard-content-container'>
-     
-      <div className='dashboard-analytics'>
-<div className='week'>
-<p>Sales</p>
-</div>
-<div className='week-one'>
-<p>Customers</p>
-</div>
-<div className='week-two'>
-<p>Orders</p>
-</div>
-<div className='marketing'>
-<p>Marketing</p>
-</div>
-<div className='products'>
-<p>All products</p>
-</div>
-<div className='cart'>
-<p>Abandoned Carts</p>
+    <div className="dashboard-content-container">
+      <div className="dashboard-analytics">
+        <div className="week">
+          <p>Sales</p>
+          <div>
 
-</div>
-<div className='orders'>
-<p>Recent Order</p>
+          </div>
+          <div>
+            <div>
 
-</div>
-<div className='summary'>
-<p>Summary</p>
-
-</div>
+            </div>
+            <div>
+              
+            </div>
+          </div>
+        </div>
+        <div className="week-one">
+          <p>Customers</p>
+        </div>
+        <div className="week-two">
+          <p>Orders</p>
+        </div>
+        <div className="marketing">
+          <p>Marketing</p>
+        </div>
+        <div className="products">
+          <p>All products</p>
+        </div>
+        <div className="cart">
+          <p>Abandoned Carts</p>
+        </div>
+        <div className="orders">
+          <p>Recent Order</p>
+        </div>
+        <div className="summary">
+          <p>Summary</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardContainer
+export default DashboardContainer;
